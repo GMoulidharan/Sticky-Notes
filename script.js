752a55e8ee68f97826b77555ab005a1cbcda62e0
GMoulidharan <moulidharansrv@gmail.com>
2024-09-21 08:36:53 +0530

    Added functionality for Add Note button and store in local storage

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const btnADD = document.getElementsByClassName('btn-add')[0];//[0] mean take the
 
 
 function getAppStorage(){
-    return JSON.parse(localStorage.getItem('sticky-app')||"[]");//JSON to array formate[obj and values].
+    return JSON.parse(localStorage.getItem('sticky-app')||"[]");//string to JSON - array formate[key and values].
 };
 
 getAppStorage().forEach(element => {
@@ -19,5 +19,26 @@ function createTextElement(id,content){
     textElement.classList.add('sticky');
     //adding value to textarea.
     textElement.value=content;
+    textElement.placeholder="Enter your notes";
     return textElement;
+}
+
+//add function
+function addSticky(){
+    const notes=getAppStorage();
+    const noteObj={
+        id:Math.floor(Math.random()*100000),//whole numbers 
+        content:"",
+    }
+    const textElement=createTextElement(noteObj.id,noteObj.content);
+    containerElement.insertBefore(textElement,btnADD);//have to store this in local storage
+    notes.push(noteObj);//here a empty content pair[array] will be stored.
+    saveNotes(notes);
+}
+
+btnADD.addEventListener('click',()=>addSticky());
+
+//save
+function saveNotes(notes){
+    localStorage.setItem('sticky-app',JSON.stringify(notes))//saving the notes in the string formate into the local storage
 }
